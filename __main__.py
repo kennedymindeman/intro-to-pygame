c765e08ae4f9b2077b4f8fbd971ef01cf5e34ff5
Kennedy Mindeman <kennedymindeman@gmail.com>
2022-08-25 23:39:27 -0500

    Add red rectangle

--- __main__.py
+++ __main__.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
 
-window_height, window_width = 800, 600
+window_height, window_width = 800, 400
 
 pygame.init()
 screen = pygame.display.set_mode((window_height, window_width))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Runner')
+
+test_surface = pygame.Surface((window_height // 8, window_width // 2))
+test_surface.fill('Red')
 
 while True:
     # update loop
@@ -15,6 +18,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(test_surface, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
